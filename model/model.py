8403ca8ee03814ef895c12f63c63bea42523dff7
Chad <chocochadstick@gmail.com>
2013-06-27 02:43:17 -0700

    Updating user model and adding Tweet model.

--- model/model.py
+++ model/model.py
@@ -7,6 +7,26 @@ from google.appengine.ext import ndb
 class User(ndb.Model):
   """Basic model for twitter users."""
   username = ndb.StringProperty()
+  last_tweet_id = ndb.StringProperty()
+  created = ndb.DateTimeProperty(auto_now_add=True)
+  updated = ndb.DateTimeProperty(auto_now=True)
+
+
+class Tweet(ndb.Model):
+  """Model representing a tweet."""
+  tweet_id = ndb.StringProperty()
+  text = ndb.StringProperty()
+  tweet_time = ndb.DateTimeProperty()
+  username = ndb.StringProperty()
+  created = ndb.DateTimeProperty(auto_now_add=True)
+  updated = ndb.DateTimeProperty(auto_now=True)
+
+  @classmethod
+  def CreateFromStatus(cls, status):
+    """Create and return an object based on a status from the Twitter API."""
+    return cls(
+        tweet_id=status.id_str, text=status.text, tweet_time=status.created_at,
+        username=status.user.screen_name)
 
 
 class UserForm(forms.Form):
