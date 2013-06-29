1c5886caa02c947a85a6d2fe028ba10ced8a2c46
Chad <chocochadstick@gmail.com>
2013-06-29 03:10:38 -0700

    Adjusting installed apps, middleware classes, etc

--- settings.py
+++ settings.py
@@ -16,23 +16,17 @@ DEBUG = local_settings.DEBUG
 TEMPLATE_DEBUG = DEBUG
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.messages',
-    'django.contrib.sessions',
 )
 
 MIDDLEWARE_CLASSES = (
     'google.appengine.ext.ndb.django_middleware.NdbDjangoMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.csrf',
     'django.core.context_processors.request',
 )
 
